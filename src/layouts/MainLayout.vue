4a9a3bbdadd2c8ae129078d07e79ad98fceccad0
hqs <imhuangqs@gmail.com>
2024-06-07 15:21:32 +0800

    update menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,6 @@
 </style>
 
 <script setup>
-import MainHeader from "./main-header.vue";
 import MainLogin from "./main-login.vue";
 </script>
 
@@ -13,7 +12,7 @@ import MainLogin from "./main-login.vue";
       <q-drawer
         class="bg-left"
         :width="280"
-        v-model="isOpen"
+        v-model="showLeft"
         show-if-above
         :breakpoint="900"
       >
@@ -30,7 +29,7 @@ import MainLogin from "./main-login.vue";
       <q-drawer
         side="right"
         :width="280"
-        v-model="isRightOpen"
+        v-model="showRight"
         show-if-above
         :breakpoint="900"
         style="background: #f2f5f9"
@@ -46,12 +45,15 @@ import MainLogin from "./main-login.vue";
 
 <script>
 import { mapState } from "vuex";
+import { useQuasar } from "quasar";
 
 export default {
   data() {
+    const { screen } = useQuasar();
+    const isOpen = screen.width > 900;
     return {
-      isOpen: false,
-      isRightOpen: false,
+      showLeft: isOpen,
+      showRight: isOpen,
     };
   },
   computed: {
@@ -62,17 +64,16 @@ export default {
     }),
   },
   mounted() {
-    this.$bus.on("toggleMenu", () => {
-      this.onToggle();
+    this.$bus.on("toggleMenu", (side) => {
+      this.onToggle(side);
     });
   },
   methods: {
-    onToggle() {
-      this.isOpen = !this.isOpen;
-    },
-    onClose() {
-      if (this.$q.screen.xs) {
-        this.isOpen = false;
+    onToggle(side) {
+      if (side == "right") {
+        this.showRight = !this.showRight;
+      } else {
+        this.showLeft = !this.showLeft;
       }
     },
   },
--- src/pages/chat/chat-header.vue
+++ src/pages/chat/chat-header.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="al-c pa-3">
-    <q-btn class="mr-2" flat dense>
+    <q-btn class="mr-2" flat dense @click="toggleMenu('left')">
       <img src="/img/ic-menu.svg" width="22px" />
     </q-btn>
     <span class="fz-18 mr-auto">Chat</span>
 
     <div class="al-c mr-1">
-      <q-btn class="ml-3" flat dense v-for="it in list" :key="it.icon">
+      <q-btn
+        class="ml-3"
+        flat
+        dense
+        v-for="it in list"
+        :key="it.icon"
+        @click="onAct(it)"
+      >
         <img :src="`/img/${it.icon}.svg`" width="22px" />
       </q-btn>
     </div>
@@ -26,9 +33,20 @@ export default {
         },
         {
           icon: "ic-robot",
+          name: "model",
         },
       ],
     };
+  },
+  methods: {
+    toggleMenu(side) {
+      this.$bus.emit("toggleMenu", side);
+    },
+    onAct({ name }) {
+      if (name == "model") {
+        this.toggleMenu("right");
+      }
+    },
   },
 };
 </script>
